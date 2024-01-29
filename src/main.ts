7c1f9bee452bae7796c0940145f1dc32b494a3ac
Samuel Ricko Perdana Putra <samuelrickop@gmail.com>
2024-01-29 23:05:12 +0700

    chore: add orm and swagger dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Dating App')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
---
+++ src/ormconfig.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+dotenv.config();
+
+const dir = process.env.NODE_ENV == 'migration' ? 'src' : 'dist';
+export default <TypeOrmModuleOptions>{
+  type: process.env.DATABASE_CONNECTION,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [`${dir}/**/*.entity.{js,ts}`],
+  migrations: [`${dir}/models/migrations/*.{js,ts}`],
+  seeds: [`${dir}/models/migrations/seeds/*.seed.{js,ts}`],
+  factories: [`${dir}/models/migrations/seeds/factories/*.factory.{js,ts}`],
+  cli: {
+    migrationsDir: `${dir}/models/migrations`,
+    entitiesDir: `${dir}/models/entities`,
+    seedersDir: `${dir}/models/migrations/seeds`,
+    factoriesDir: `${dir}/models/migrations/seeds/factories`,
+  },
+  synchronize: false,
+};
